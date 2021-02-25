407cfcc0321b77dfb67958d4a68ffb62f1e17698
IsmaeElias <ismael.eliass22@gmail.com>
2021-02-24 23:46:51 -0300

    finalizando countdown

---
+++ src/components/CompleteChallenges/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/components/CompleteChallenges.module.css';
+
+
+export function CompleteChallenges(){
+  return(
+    <div className={styles.CompleteChallengesContainer}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  );
+}
